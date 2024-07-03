ccabbb967ecc670dc36f9e893b42342a242b84eb
user <user@uzchasys-1522-4.chasys.local>
2024-07-03 17:46:31 +0500

    commit 2

--- lib/screens/lesson11/home_work11.dart
+++ lib/screens/lesson11/home_work11.dart
@@ -6,6 +6,25 @@ class HomeWork11 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: null,
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.business),
+      //       label: null,
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.school),
+      //       label: null,
+      //     ),
+      //   ],
+      //   // currentIndex: _selectedIndex,
+      //   // selectedItemColor: Colors.amber[800],
+      //   // onTap: _onItemTapped,
+      // ),
       appBar: AppBar(
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -35,109 +54,123 @@ class HomeWork11 extends StatelessWidget {
       ),
       body: Container(
         // height: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(children: [
-                  CircleAvatar(
-                    radius: 43,
-                    backgroundColor: Color.fromARGB(172, 184, 33, 22),
-                    child: CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(
-                            'https://randomuser.me/api/portraits/women/74.jpg')),
-                  ),
-                  Text("Your story",
-                      style: TextStyle(fontWeight: FontWeight.w500))
-                ]),
-        
-                Column(children: [
-                  CircleAvatar(
-                    radius: 43,
-                    backgroundColor: Color.fromARGB(172, 184, 33, 22),
-                    child: CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(
-                            'https://randomuser.me/api/portraits/women/64.jpg')),
-                  ),
-                  Text("Your story",
-                      style: TextStyle(fontWeight: FontWeight.w500))
-                ]),
-        
-                Column(children: [
-                  CircleAvatar(
-                    radius: 43,
-                    backgroundColor: Color.fromARGB(172, 184, 33, 22),
-                    child: CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(
-                            'https://randomuser.me/api/portraits/women/54.jpg')),
-                  ),
-                  Text("Your story",
-                      style: TextStyle(fontWeight: FontWeight.w500))
-                ]),
-        
-                Column(children: [
-                  CircleAvatar(
-                    radius: 43,
-                    backgroundColor: Color.fromARGB(172, 184, 33, 22),
-                    child: CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(
-                            'https://randomuser.me/api/portraits/women/34.jpg')),
-                  ),
-                  Text("Your story",
-                      style: TextStyle(fontWeight: FontWeight.w500))
-                ]),
-              ],
-            ),  
             Container(
-            height: 80,
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width*0.12,
-                  child: const CircleAvatar(
-                    radius: 25,
-                    backgroundImage: NetworkImage(
-                        'https://randomuser.me/api/portraits/women/88.jpg')),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width*0.65,
-                  // height: 100,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        child: Row(
-                          children: [
-                            Text("jousha_Id", style: TextStyle(fontWeight: FontWeight.w600),),
-                            Text(" "),
-                            Icon(Icons.task, size: 14, color: Colors.blue,),
-                          ],
-                        ),
-                      ),
-                      Text("Tokyo, Japan"),
-                    ],
-                  ),
-                ),
-                const Icon(
-                  Icons.more_horiz,
-                  size: 26,
-                )
-              ],
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Color.fromARGB(172, 184, 33, 22),
+                      child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://randomuser.me/api/portraits/women/74.jpg')),
+                    ),
+                    Text("Your story",
+                        style: TextStyle(fontWeight: FontWeight.w500))
+                  ]),
+                  Column(children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Color.fromARGB(172, 184, 33, 22),
+                      child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://randomuser.me/api/portraits/women/64.jpg')),
+                    ),
+                    Text("Your story",
+                        style: TextStyle(fontWeight: FontWeight.w500))
+                  ]),
+                  Column(children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Color.fromARGB(172, 184, 33, 22),
+                      child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://randomuser.me/api/portraits/women/54.jpg')),
+                    ),
+                    Text("Your story",
+                        style: TextStyle(fontWeight: FontWeight.w500))
+                  ]),
+                  Column(children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Color.fromARGB(172, 184, 33, 22),
+                      child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://randomuser.me/api/portraits/women/34.jpg')),
+                    ),
+                    Text("Your story",
+                        style: TextStyle(fontWeight: FontWeight.w500))
+                  ]),
+                ],
+              ),
             ),
-          ),       
-
+            Container(
+              height: 80,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.12,
+                    child: const CircleAvatar(
+                        radius: 25,
+                        backgroundImage: NetworkImage(
+                            'https://randomuser.me/api/portraits/women/88.jpg')),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    // height: 100,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          child: Row(
+                            children: [
+                              Text(
+                                "jousha_Id",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                              Text(" "),
+                              Icon(
+                                Icons.task,
+                                size: 14,
+                                color: Colors.blue,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text("Tokyo, Japan"),
+                      ],
+                    ),
+                  ),
+                  const Icon(
+                    Icons.more_horiz,
+                    size: 26,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: Image.asset(
+                "assets/images/Rectangle.png",
+                fit: BoxFit.cover,
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
